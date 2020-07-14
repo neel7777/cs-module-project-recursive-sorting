19e4b9d665295b8bfb76bcf233d67e8b07f999ce
Neel Mazumdar <neelmazumdar7@gmail.com>
2020-07-13 21:35:54 -0400

    first attempts

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -4,6 +4,8 @@ def merge(arrA, arrB):
     merged_arr = [0] * elements
 
     # Your code here
+    merged_arr.append(arrA)
+    merged_arr.append(arrB)
 
 
     return merged_arr
@@ -19,10 +21,10 @@ def merge_sort(arr):
 # utilize any extra memory
 # In other words, your implementation should not allocate any additional lists 
 # or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# def merge_in_place(arr, start, mid, end):
+#     # Your code here
 
 
-def merge_sort_in_place(arr, l, r):
-    # Your code here
+# def merge_sort_in_place(arr, l, r):
+#     # Your code here
 
